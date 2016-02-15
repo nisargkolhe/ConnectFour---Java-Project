35883a5f43871d63371ff9e1fb6a02942c8e06e1
nisargkolhe <nisargkolhe@gmail.com>
2016-02-15 20:43:26 +0530

    Last commit. Too late, oops.

---
+++ src/classes/pclass.java
@@ -0,0 +1,22 @@
+package classes;
+
+import javax.swing.JOptionPane;
+
+
+
+public class pclass {
+    
+    //Data member
+    public static int players,turn;
+    public static int a[] = new int[] {0,0,0,0,0,0,0};;
+    public static int b[] = new int[] {0,0,0,0,0,0,0};;
+    public static int c[] = new int[] {0,0,0,0,0,0,0};;
+    public static int d[] = new int[] {0,0,0,0,0,0,0};;
+    public static int e[] = new int[] {0,0,0,0,0,0,0};;
+    public static int f[] = new int[] {0,0,0,0,0,0,0};;
+    public static int g[] = new int[] {0,0,0,0,0,0,0};;
+    public static String p1 = "Player 1";
+    public static String p2 = "Player 2";
+    public static String p2c = "";
+    public static String p1c = "";
+}
